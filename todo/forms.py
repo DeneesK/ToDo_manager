3dc42b4c49224c27c04e7e8299c68dc3ea83b85c
D Kalb <kds-ru@list.ru>
2022-07-13 11:11:46 +0200

    Changed todo's creation form and added change welcome message on homepage after login

--- todo/forms.py
+++ todo/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
 from .models import ToDo
+from django import forms
 
 class FormTodo(ModelForm):
     class Meta:
         model = ToDo
         fields = ['title', 'memo', 'important']
+        widgets = {
+            'memo': forms.Textarea(attrs={"class": "form-control"}),
+            'title': forms.TextInput(attrs={"class": "form-control"}) 
+        }
--- todo/models.py
+++ todo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.forms import CharField
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class ToDo(models.Model):
     title = models.CharField(max_length=100)
